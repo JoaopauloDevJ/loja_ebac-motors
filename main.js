18f0cf40f6c5e0be66e54c625bcfee32f5f4f7cd
João Paulo Santos de Souza <joaopaulo.santos_souza@hotmail.com>
2023-11-18 10:28:57 -0300

    commit initial - Loja criada por mim em um dos projetos da EBAC

---
+++ main.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+    $("#carousel-imagens").slick({
+        autoplay:true,
+    });
+    $(".menu-hamburguer").click(function (){
+        $("nav").slideToggle();
+    })
+    $("#telefone").mask("(00) 00000-0000", {
+        placeholder:"(__) _____-____"
+    })
+    $("form").validate({
+        rules:{
+            nome:{
+                required:true
+            },
+            email:{
+                required:true,
+                email:true
+            },
+            telefone:{
+                required:true
+            },
+            veiculo:{
+                required:false
+            },
+            mensagem:{
+                required:true
+            }
+        },
+        messages:{
+            nome:"Por favor, insira seu nome!",
+            email:"Por favor, digite seu e-mail!",
+            telefone:"Por favor, digite seu número de telefone!",
+            mensagem:"Por favor, escreva uma mensagem!"
+        },
+        submitHandler: function(form) {
+            console.log(form)
+        },
+        invalidHandler: function(evento, validador){
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos){
+                alert(`existe ${camposIncorretos} campos incorretos!`)
+            }
+        }
+    })
+    $(".lista-veiculos button").click(function(){
+        const destino = $("#contato");
+
+        const nomeVeiculo = $(this).parent().find("h3").text();
+
+        $("#veiculo").val(nomeVeiculo);
+
+        $("html").animate({
+            scrollTop: destino.offset().top
+        }, 1000)
+    })
+})
